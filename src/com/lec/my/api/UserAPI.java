007cb98defb0fb600639233574a06a6493c945d2
Dean <>
2020-12-26 21:48:50 +0900

    arrange code

--- src/com/lec/my/api/UserAPI.java
+++ src/com/lec/my/api/UserAPI.java
@@ -14,65 +14,65 @@ public class UserAPI {
 	private static final String PERSISTENCE_UNIT_NAME = "h2";
 	private static final EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
 	private static final EntityManager em = factory.createEntityManager();
-	
+
 	public boolean register(String id, String name, String password) {
 		try {
 			User user = new User();
 			user.setId(id);
 			user.setName(name);
 			user.setPassword(password);
-			
+
 			EntityTransaction transaction = em.getTransaction();
-	        transaction.begin(); 
-	        em.persist(user);
-	        transaction.commit();
+			transaction.begin();
+			em.persist(user);
+			transaction.commit();
 		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
-		} 
+		}
 		return true;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public boolean login(String id, String pwd) {
 		Query query = em.createQuery("select t from User t where id = " + id + " and password = " + pwd);
-        List<User> resultList = query.getResultList();
-        
-        if(resultList.size() == 1)
-        	return true;
-        else 
-        	return false;
+		List<User> resultList = query.getResultList();
+
+		if (resultList.size() == 1)
+			return true;
+		else
+			return false;
 	}
-	
+
 	public boolean update(String id, String name, String password, String address) {
 		try {
 			User user = em.find(User.class, id);
-			
+
 			EntityTransaction transaction = em.getTransaction();
-	        transaction.begin(); 
+			transaction.begin();
 			user.setName(name);
 			user.setPassword(password);
 			user.setAddress(address);
 			transaction.commit();
-	        
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
-		} 
+		}
 		return true;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public User read(String id) {
 		Query query = em.createQuery("select u from User u where id = " + id);
-        List<User> resultList = query.getResultList();
-        
-        if(resultList.size() == 1)
-        	return resultList.get(0);
-        else 
-        	return null;
+		List<User> resultList = query.getResultList();
+
+		if (resultList.size() == 1)
+			return resultList.get(0);
+		else
+			return null;
 	}
-	
+
 	public List<User> readAll() {
 		return null;
 	}
