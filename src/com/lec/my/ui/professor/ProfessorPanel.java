c5707a12160b1fd0365f5de089502c6ab4247247
Dean <>
2021-01-09 15:47:19 +0900

    add swing code

---
+++ src/com/lec/my/ui/professor/ProfessorPanel.java
@@ -0,0 +1,7 @@
+package com.lec.my.ui.professor;
+
+import javax.swing.JFrame;
+
+public class ProfessorPanel extends JFrame {
+
+}
---
+++ src/com/lec/my/ui/student/StudentPanel.java
@@ -0,0 +1,7 @@
+package com.lec.my.ui.student;
+
+import javax.swing.JFrame;
+
+public class StudentPanel extends JFrame {
+
+}
